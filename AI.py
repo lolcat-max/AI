79367d7ae2798f2f65c80e499b4a3207c72d1c03
lolcat-max <steamlolcat0@gmail.com>
2025-12-24 13:55:51 +1100

    v5.0.1

--- AI.py
+++ AI.py
@@ -63,7 +63,7 @@ class TextDataset(Dataset):
 # -------------------------
 # 3. Generation & Inference
 # -------------------------
-def generate_text(model, inverter, seed, w2i, i2w, seq_len, max_len=500, choose_words=None):
+def generate_text(model, seed, w2i, i2w, seq_len, max_len=500, choose_words=None):
     model.eval()
     gen_ids = [w2i.get(w, 0) for w in seed.split()]
     
@@ -146,4 +146,4 @@ if __name__ == "__main__":
     while True:
         seed = input("\nSeed >> ").strip().lower()
         if not seed: break
-        generate_text(model, inverter, seed, w2i, i2w, SEQ_LEN)
+        generate_text(model, seed, w2i, i2w, SEQ_LEN)
