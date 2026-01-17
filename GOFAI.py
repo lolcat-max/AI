a5bf8465c60e617ff1a004ae6278ecdf15f8580d
lolcat-max <steamlolcat0@gmail.com>
2026-01-18 09:59:06 +1100

    Update GOFAI.py

--- GOFAI.py
+++ GOFAI.py
@@ -79,7 +79,7 @@ class NltkSemanticKB:
     LEXNAME_OVERRIDES = {
         "noun.time": "when",
         "noun.motive": "why",
-        "noun.possession": "why",  # money, reward
+        "noun.possession": "why", 
         "noun.feeling": "feel",
         "noun.act": "how",
     }
